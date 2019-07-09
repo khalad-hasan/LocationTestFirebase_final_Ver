11863b0d4a9caaffff5d8de528a48a4ec7fc42c2
zhk <k.zilehuma@yahoo.in>
2019-07-08 17:11:30 -0700

    Notification Listener

--- app/src/main/java/com/example/user/locationtestfirebase/NotificationService.java
+++ app/src/main/java/com/example/user/locationtestfirebase/NotificationService.java
@@ -12,6 +12,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class NotificationService extends NotificationListenerService {
+    private static final String TAG = TrackingService.class.getSimpleName();
    Context context;
 
     @Override
@@ -33,8 +34,9 @@ public class NotificationService extends NotificationListenerService {
         String title = "";
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
             Bundle extras = extras = sbn.getNotification().extras;
-            text = extras.getCharSequence("android.text").toString();
-            title = extras.getString("android.title");
+        //    text = extras.getCharSequence("android.text").toString();
+         //   title = extras.getString("android.title");
+
         }
         String date = String.valueOf(android.text.format.DateFormat.format("dd-MM-yyyy HH:mm:ss", new java.util.Date()));
         String noti = date + "\t" + pack   + "\n";
@@ -51,13 +53,30 @@ public class NotificationService extends NotificationListenerService {
             e.printStackTrace();
         }
         Log.i("Package",pack);
-        Log.i("Title",title);
-        Log.i("Text",text);
+     //   Log.i("Title",title);
+     //   Log.i("Text",text);
 
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
+        String pack = sbn.getPackageName();
+        String date = String.valueOf(android.text.format.DateFormat.format("dd-MM-yyyy HH:mm:ss", new java.util.Date()));
+        String noti = date + "\t" + pack   + "\n";
+        try {
+            File data7 = new File("notificationrem.txt");
+            FileOutputStream fos = openFileOutput("notificationrem.txt", Context.MODE_APPEND);
+            fos.write((noti).getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         Log.i("Msg","Notification was removed");
     }
+
+
 }
